ad9cdce46cd9affd7aef21cbfbdd91d8521a8d20
jp26jp <john@j-26.com>
2025-06-01 02:55:47 -0600

    Bump version to 1.0.3

--- open_to_close_api/__init__.py
+++ open_to_close_api/__init__.py
@@ -23,7 +23,7 @@ from .tags import TagsAPI
 from .teams import TeamsAPI
 from .users import UsersAPI
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __all__ = [
     "OpenToCloseAPI",
     # Service Clients
