3f1e09f8706ae681819693e8469ccb77ba7727c2
jp26jp <john@j-26.com>
2025-06-01 08:52:16 -0600

    Version bump to 2.2.6 - Maintenance release with updated packaging workflow

--- open_to_close/__init__.py
+++ open_to_close/__init__.py
@@ -23,7 +23,7 @@ from .tags import TagsAPI
 from .teams import TeamsAPI
 from .users import UsersAPI
 
-__version__ = "2.2.5"
+__version__ = "2.2.6"
 __all__ = [
     "OpenToCloseAPI",
     # Service Clients
